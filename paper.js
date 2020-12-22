c9dcf7429f8c6734429ad32822df30fcef7c002f
ZidaneAli123 <71212869+ZidaneAli123@users.noreply.github.com>
2020-12-22 13:52:24 +0530

    Update paper.js

--- paper.js
+++ paper.js
@@ -14,7 +14,7 @@ class paper
 		this.y=y;
 		this.r=r
 		this.body=Bodies.circle(this.x, this.y, this.r/2, options)
-		this.image='paper.png'
+		this.image= loadImage('paper.png')
 		World.add(world, this.body);
 
 	}
